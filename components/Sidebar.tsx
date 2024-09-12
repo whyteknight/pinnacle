13c480d269f1703dc7bd6a78544e5c680857cca1
whyteknight <michael1981torres@gmail.com>
2024-09-12 18:21:00 +0800

    first commit on banking app

---
+++ components/Sidebar.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import Footer from "./Footer";
+import PlaidLink from "./PlaidLink";
+
+const Sidebar = ({ user }: SiderbarProps) => {
+  const pathname = usePathname();
+  return (
+    <section className="sidebar transition-all duration-500 ease-in-out shadow-md z-10">
+      <nav className="flex flex-col gap-4">
+        <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
+          <Image
+            src="/icons/bank-logo.png"
+            alt="Pinnacle logo"
+            width={40}
+            height={30}
+            className="max-xl:size-20"
+          />
+          <h1 className="sidebar-logo">Pinnacle</h1>
+        </Link>
+
+        {sidebarLinks.map((item) => {
+          const isActive =
+            pathname === item.route || pathname.startsWith(`${item.route}/`);
+          return (
+            <Link
+              href={item.route}
+              key={item.label}
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
+            >
+              <div className="relative size-6">
+                <Image
+                  src={item.imgURL}
+                  alt={item.label}
+                  fill
+                  className={cn({
+                    "brightness-[3] invert-0": isActive,
+                  })}
+                />
+              </div>
+              <p
+                className={cn("sidebar-label", {
+                  "!text-white": isActive,
+                })}
+              >
+                {item.label}
+              </p>
+            </Link>
+          );
+        })}
+
+        <PlaidLink user={user} />
+      </nav>
+
+      <Footer user={user} />
+    </section>
+  );
+};
+
+export default Sidebar;
